c120fbff836f9181edc262f53edca7d00cf73974
zzzz <QijingZeng02@163.com>
2024-12-16 19:58:48 +0000

    Update main01.ino
    Using method 1 as counting logic and controlling light color based on how many people in the room

--- main01.ino
+++ main01.ino
@@ -2,12 +2,7 @@
 #include <PubSubClient.h>
 #include "arduino_secrets.h"
 
-const int trigPin1 = 2;
-const int echoPin1 = 3;
-const int trigPin2 = 4;
-const int echoPin2 = 5;
-
-// MQTT setting
+// WiFi and MQTT settings
 const char* ssid = SECRET_SSID;
 const char* password = SECRET_PASS;
 const char* mqtt_username = SECRET_MQTTUSER;
@@ -15,18 +10,25 @@ const char* mqtt_password = SECRET_MQTTPASS;
 const char* mqtt_server = "mqtt.cetools.org";
 const int mqtt_port = 1884;
 
+// Ultrasonic sensor pins
+const int trigPin1 = 6;
+const int echoPin1 = 7;
+const int trigPin2 = 4;
+const int echoPin2 = 5;
+
+// MQTT topic
+char mqtt_topic_demo[] = "student/CASA0014/light/2/pixel/";
+
+// Variables to store the state of the sensors and the count of people
+int peopleCount = 0;
+bool u1 = false;
+bool u2 = false;
+
 WiFiClient mkrClient;
 PubSubClient client(mkrClient);
 
-char mqtt_topic_demo[] = "student/CASA0014/light/26/pixel/";
-
-int personCount = 0; // counter
-bool wasSensor1Triggered = false;
-bool wasSensor2Triggered = false;
-
 void setup() {
   Serial.begin(115200);
-  delay(1000);
   pinMode(trigPin1, OUTPUT);
   pinMode(echoPin1, INPUT);
   pinMode(trigPin2, OUTPUT);
@@ -44,64 +46,64 @@ void loop() {
   }
   client.loop();
 
-  int distance1 = measureDistance(trigPin1, echoPin1);
-  int distance2 = measureDistance(trigPin2, echoPin2);
+  checkUltrasonicSensor(trigPin1, echoPin1);
+  checkUltrasonicSensor(trigPin2, echoPin2);
 
-  Serial.print("Distance A: ");
-  Serial.println(distance1);
-  Serial.print("Distance B: ");
-  Serial.println(distance2);
-
-    bool isSensor1Triggered = distance1 < 20;
-  bool isSensor2Triggered = distance2 < 20;
+  bool isSensor1Triggered = u1;
+  bool isSensor2Triggered = u2;
 
   // detect change
-  checkPersonCount(isSensor1Triggered, isSensor2Triggered, wasSensor1Triggered, wasSensor2Triggered);
+  checkPersonCount(isSensor1Triggered, isSensor2Triggered);
 
-  wasSensor1Triggered = isSensor1Triggered;
-  wasSensor2Triggered = isSensor2Triggered;
+  // Reset sensor triggered flags
+  u1 = false;
+  u2 = false;
 
-  Serial.print("There are");
-  Serial.print(personCount);
-  Serial.println("people in the room");
+  Serial.print("There are ");
+  Serial.print(peopleCount);
+  Serial.println(" people in the room");
 
   sendCurrentLightState();
-  delay(1000);
+  delay(500);
 }
 
-void checkPersonCount(bool sensor1Triggered, bool sensor2Triggered, bool wasSensor1Triggered, bool wasSensor2Triggered) {
-  if (sensor1Triggered && !wasSensor1Triggered && !sensor2Triggered) {
-    // The firse sensor detected and the second sensor does not detected, defining there was a person enter room
-    personCount++;
-  } else if (!sensor1Triggered && wasSensor1Triggered && sensor2Triggered) {
-    // The firse sensor does not detected and the second sensor detected, defining there was a person exit room
-    if (personCount > 0) {
-      personCount--;
+void checkUltrasonicSensor(int trigPin, int echoPin) {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  long duration = pulseIn(echoPin, HIGH);
+  float distance = duration * 0.034 / 2;
+
+  if (distance < 5) {
+    if (trigPin == trigPin1) {
+      u1 = true;
+    } else if (trigPin == trigPin2) {
+      u2 = true;
     }
   }
 }
 
-void updateLightColor() {
-  if (personCount == 0) {
-    sendmqttOff();
-  } else if (personCount >= 1 && personCount <= 9) {
-    sendmqttRed();
-  } else if (personCount >= 10 && personCount <= 19) {
-    sendmqttYellow();
-  } else if (personCount >= 20 && personCount <= 30) {
-    sendmqttGreen();
+void checkPersonCount(bool sensor1Triggered, bool sensor2Triggered) {
+  if (sensor1Triggered && !sensor2Triggered) {
+    peopleCount++;
+  } else if (!sensor1Triggered && sensor2Triggered) {
+    if (peopleCount > 0) {
+      peopleCount--;
+    }
   }
 }
 
 void sendCurrentLightState() {
-  if (personCount >= 1 && personCount <= 9) {
-    sendmqttRed();
-  } else if (personCount >= 10 && personCount <= 19) {
-    sendmqttYellow();
-  } else if (personCount >= 20 && personCount <= 30) {
-    sendmqttGreen();
-  } else {
+  if (peopleCount == 0) {
     sendmqttOff();
+  } else if (peopleCount >= 1 && peopleCount <= 9) {
+    sendmqttRed();
+  } else if (peopleCount >= 10 && peopleCount <= 19) {
+    sendmqttYellow();
+  } else if (peopleCount >= 20) {
+    sendmqttGreen();
   }
 }
 
@@ -137,16 +139,6 @@ void setLightColor(int r, int g, int b) {
       Serial.println("Failed to publish message");
     }
   }
-}
-
-int measureDistance(int trigPin, int echoPin) {
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(2);
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
-  int duration = pulseIn(echoPin, HIGH);
-  return duration * 0.034 / 2;
 }
 
 void startWifi() {
